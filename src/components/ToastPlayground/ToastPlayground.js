085976ba55cea7ec906412ad6320d2dee44ead4e
mikuweb <mxxx.kwtmg@gmail.com>
2023-04-16 17:52:38 +0900

    implement live-editable toast preview

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { Children, useState } from "react";
 
 import Button from "../Button";
 
 import styles from "./ToastPlayground.module.css";
+import Toast from "../Toast/Toast";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
   const [message, setMessage] = useState("");
   const [variant, setVariant] = useState(VARIANT_OPTIONS[0]);
+  const [isRender, setIsRender] = useState(false);
+
+  const handleDismiss = () => {
+    setIsRender(false);
+    setMessage("");
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +23,11 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+      {isRender && (
+        <Toast variant={variant} handleDismiss={handleDismiss}>
+          {message}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -65,7 +77,7 @@ function ToastPlayground() {
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
             <Button
               onClick={() => {
-                window.alert(`${message}, ${variant}`);
+                setIsRender(true);
               }}
             >
               Pop Toast!
