da4288333dab70ba15ca9ce744b550ec854d9035
mikuweb <mxxx.kwtmg@gmail.com>
2023-04-19 15:54:05 +0900

    implement keyboard and screen reader support

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const ToastContext = React.createContext();
 
-function ToastProvider({ children }) {
+const ToastProvider = ({ children }) => {
   const [toasts, setToasts] = useState([]);
 
   const createToast = (message, variant) => {
@@ -22,11 +22,25 @@ function ToastProvider({ children }) {
     setToasts(newToasts);
   };
 
+  useEffect(() => {
+    const handleKeydown = (e) => {
+      if (e.code === "Escape") {
+        setToasts([]);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
   return (
     <ToastContext.Provider value={{ toasts, createToast, dismissToast }}>
       {children}
     </ToastContext.Provider>
   );
-}
+};
 
 export default ToastProvider;
